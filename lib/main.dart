683097fd4d597f47e22e24c35a6d8699be9107f4
Thura Aung <thura.ucsy@gmail.com>
2019-08-22 16:04:26 +0900

    package use > contact read

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:contacts_service/contacts_service.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State {
+  List<Contact> _contacts = [];
+
+  Future<PermissionStatus> _getContactPermission() async {
+    PermissionStatus permission = await PermissionHandler().checkPermissionStatus(PermissionGroup.contacts);
+    if (permission != PermissionStatus.granted && permission != PermissionStatus.disabled) {
+      Map<PermissionGroup, PermissionStatus> permissionStatus = await PermissionHandler().requestPermissions([PermissionGroup.contacts]);
+      return permissionStatus[PermissionGroup.contacts] ?? PermissionStatus.unknown;
+    } else {
+      return permission;
+    }
+  }
+
+  refreshContact() async {
+    PermissionStatus permissionStatus = await _getContactPermission();
+    if (permissionStatus == PermissionStatus.granted) {
+      Iterable<Contact> contacts = await ContactsService.getContacts();
+      setState(() {
+        _contacts = contacts.toList();
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    refreshContact();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,9 +48,15 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Welcome to Flutter'),
         ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
+        body: ListView.builder(
+            itemCount: _contacts.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(_contacts.elementAt(index).displayName),
+                subtitle: Text(_contacts.elementAt(index).phones.length > 0 ? _contacts.elementAt(index).phones.elementAt(0).value : ''),
+              );
+            }
+        )
       ),
     );
   }
