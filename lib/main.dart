11964fa891a9a6d9b166fc5b62c7ace683b4b5d5
Thura Aung <thura.ucsy@gmail.com>
2019-08-22 16:14:55 +0900

    font output > embed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:contacts_service/contacts_service.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:zawgyi_converter/zawgyi_converter.dart';
+import 'package:zawgyi_converter/zawgyi_detector.dart';
+
+ZawgyiDetector zawgyiDetector = ZawgyiDetector();
+ZawgyiConverter zawgyiConverter = ZawgyiConverter();
 
 void main() => runApp(MyApp());
 
@@ -12,13 +17,18 @@ class MyApp extends StatefulWidget {
 }
 
 class MyAppState extends State {
-  List<Contact> _contacts = [];
+  List<Contact> _contacts;
 
   Future<PermissionStatus> _getContactPermission() async {
-    PermissionStatus permission = await PermissionHandler().checkPermissionStatus(PermissionGroup.contacts);
-    if (permission != PermissionStatus.granted && permission != PermissionStatus.disabled) {
-      Map<PermissionGroup, PermissionStatus> permissionStatus = await PermissionHandler().requestPermissions([PermissionGroup.contacts]);
-      return permissionStatus[PermissionGroup.contacts] ?? PermissionStatus.unknown;
+    PermissionStatus permission = await PermissionHandler()
+        .checkPermissionStatus(PermissionGroup.contacts);
+    if (permission != PermissionStatus.granted &&
+        permission != PermissionStatus.disabled) {
+      Map<PermissionGroup, PermissionStatus> permissionStatus =
+          await PermissionHandler()
+              .requestPermissions([PermissionGroup.contacts]);
+      return permissionStatus[PermissionGroup.contacts] ??
+          PermissionStatus.unknown;
     } else {
       return permission;
     }
@@ -28,8 +38,17 @@ class MyAppState extends State {
     PermissionStatus permissionStatus = await _getContactPermission();
     if (permissionStatus == PermissionStatus.granted) {
       Iterable<Contact> contacts = await ContactsService.getContacts();
+
+      List<Contact> zawgyiContacts = [];
+      contacts.map((c) {
+        if (zawgyiDetector.predict(c.displayName) > 0.05) {
+          c.displayName = zawgyiConverter.zawgyiToUnicode(c.displayName);
+          zawgyiContacts.add(c);
+        }
+      }).toList();
+
       setState(() {
-        _contacts = contacts.toList();
+        _contacts = zawgyiContacts;
       });
     }
   }
@@ -45,19 +64,24 @@ class MyAppState extends State {
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: ListView.builder(
-            itemCount: _contacts.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(_contacts.elementAt(index).displayName),
-                subtitle: Text(_contacts.elementAt(index).phones.length > 0 ? _contacts.elementAt(index).phones.elementAt(0).value : ''),
-              );
-            }
-        )
-      ),
+          appBar: AppBar(
+            title: Text('Welcome to Flutter'),
+          ),
+          body: (_contacts == null)
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : ListView.builder(
+                  itemCount: _contacts.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(_contacts.elementAt(index).displayName, style: TextStyle(fontFamily: 'masterpiece'),),
+                      subtitle: Text(_contacts.elementAt(index).phones.length >
+                              0
+                          ? _contacts.elementAt(index).phones.elementAt(0).value
+                          : ''),
+                    );
+                  })),
     );
   }
 }
